faf50acdff5d2ccab140989dbde17a510d54d7a6
Luis Mayta <luis.mayta@gmail.com>
2018-04-24 19:22:00 -0500

    listados

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -24,6 +24,9 @@ class DefaultController extends ControllerBase {
     $data['anio'] = $year;
     $data['mes'] = $mes;
 
+    $result = $this->getData('tareas');
+    $data['ingresos'] = $result;
+    $data['suma'] = $suma;
     return [
       '#theme' => 'module_caja',
       '#data' => $data,
@@ -48,6 +51,30 @@ class DefaultController extends ControllerBase {
         ['value'=>11,'title'=>'Noviembre'],
         ['value'=>12,'title'=>'Diciembre'],
     ];
+  }
+  public function getData($tipo)
+  {
+    $query = db_select('node_field_data', 'nfd')->distinct();
+             $query->fields('nfd',['nid','title']);
+             $query->addField('nfc','field_cantidad_value');
+             $query->join('node__field_cantidad','nfc','nfc.entity_id = nfd.nid');
+             $query->condition('nfd.type',$tipo);
+
+    $query = $query->execute()->fetchAll();
+    $result = [];
+    $suma = 0;
+
+    foreach ($query as $key => $item) {
+       array_push($result,[
+                'nid' => $item->nid,
+                'title' => $item->title,
+                'cantidad' => $item->field_cantidad_value,
+            ]);
+       $suma += $item->field_cantidad_value;
+    }
+    return[
+      'data' => $result,
+    ]
   }
 
 
